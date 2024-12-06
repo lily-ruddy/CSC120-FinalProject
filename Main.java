1e2415933a94a34d7f8d406b970907118d2feaab
Lily Ruddy <lruddy@smith.edu>
2024-12-06 15:39:02 -0500

    fixed some things, might not be able to generalize, may have repeating code

--- Main.java
+++ Main.java
@@ -23,19 +23,21 @@ public class Main{
         Room mainBedroom = new Room("Main Bedroom", "Looking around you notice that the bedroom is very sparse. There's a metal framed bed, a desk with a single candle lighting up the room, and a wooden chair. A large window takes up most of the whole wall. There's two sturdy doors leading out; one that is white and the other is black.", false);
         Room mainBathroom = new Room("Main Bathroom", "Observing the bathroom you see a sleak bathtub, a modest sink and toilet. There's a mirror above the sink. You don't recognize the reflection staring back at you. You are confused. There lays a single tube of toothpaste and a toothbrush innocently laying besides the sink. ", false);
         Room hallway = new Room("Hallway", "You step into a long hallway with six doors.", false);
-        
-        /* Sub Rooms */ // areas found inside the Rooms
-        SubRoom desk = new SubRoom("desk", mainBedroom, "The desk looks pretty standard with no extra compartments. There is a single sheet of blank paper atop the desk, but as you move closer you notice lettering in dark purplpe ink slowly appear: ESCAPE!", "The desk looks pretty standard with no extra compartments.", false, false);
-        SubRoom window = new SubRoom("window", mainBedroom ,"You move closer to the window and the scenery becomes clearer before you. You realize that you are several thousands of feet above the ground with miles and miles of a snowy landscape as far as the eye can see. ", null, false, true);
 
         /* Objects */
         Item deskPaper = new Item("Paper", "A piece of paper found on the desk of the Main Bedroom. 'ESCAPE!' is written on the paper in some special dark purple ink.", false);
         Item toothBrush = new Item("Toothbrush", "It's an orange toothbrush that appears to be unused.", false);
         Item toothPaste = new Item("Toothpaste", "A drugstore brand toothpaste: 6/10 DENTISTS AGREE THAT THIS IS IN FACT TOOTHPASTE", false);
         
+        /* Sub Rooms */ // areas found inside the Rooms
+        SubRoom desk = new SubRoom("desk", mainBedroom, "The desk looks pretty standard with no extra compartments. There is a single sheet of blank paper atop the desk, but as you move closer you notice lettering in dark purplpe ink slowly appear: ESCAPE!", "The desk looks pretty standard with no extra compartments.", false, false, deskPaper, null);
+        SubRoom window = new SubRoom("window", mainBedroom ,"You move closer to the window and the scenery becomes clearer before you. You realize that you are several thousands of feet above the ground with miles and miles of a snowy landscape as far as the eye can see. ", null, false, true, null, null);
+        //List<SubRoom> subRoomsList = Arrays.asList(window, desk);
+
         /* Other */
         Person user = new Person(inventory, mainBedroom);
-        List<SubRoom> subRoomsList = Arrays.asList(window, desk);
+        
+        
 
         // START OF THE GAME:
         /* Intro to Game */
@@ -92,43 +94,49 @@ public class Main{
             // Inside Main Bedroom:
             if(user.getRoom().getName().equals("Main Bedroom")){
 
-                /* Go to window or desk if it's unlocked */
-                for(SubRoom i:subRoomsList){
-                    if(actionsList.contains(userWords[0]) && userResponse.contains(i.getName()) && i.getLocked() == false){
-                        System.out.println(i.getDescription());
+            
+                // for(SubRoom i:subRoomsList){
+                //     if(actionsList.contains(userWords[0]) && userResponse.contains(i.getName()) && i.getLocked() == false){
+                //         System.out.println(i.getDescription());
 
-                        /* If the subroom has an object, allow access to it */
-                        if(i.getEmpty() == false){
+                //         /* If the subroom has an object, allow access to it */
+                //         if(i.getEmpty() == false && i.getItemList() != null){
+                //             i.getItem().setAccess(true); 
+                //             System.out.println(i.getItem().getName());
+                //             System.out.println(i.getItem().getAccess());
+                //         } 
+                //     }
+                // }
 
-                        }
-
-                    
+                // /* Pick up object */
+                // for(SubRoom i:subRoomsList){
+                //     if(actionsList.contains(userWords[0]) && userResponse.contains(i.getName())){
+                //         System.out.println(i.getDescription());
                         
-                    }
-                }
+                //     }
+                // }
 
-                /* Pick up object */
-                for(SubRoom i:subRoomsList){
-                    if(actionsList.contains(userWords[0]) && userResponse.contains(i.getName())){
-                        System.out.println(i.getDescription());
-                        
-                    }
+
+                /* Go to window */
+                if(actionsList.contains(userWords[0]) && userResponse.contains(window.getName())){
+                    System.out.println(window.getDescription());
                 }
 
 
                 /* Go to desk */
-                // if(actionsList.contains(userWords[0]) && userResponse.contains(desk.getName())){
-                //     System.out.println(desk.getDescription());
-                //     deskPaper.setAccess(true); // allows user to pick up the paper
-
-                //     // if(user)
-                // }
+                if(actionsList.contains(userWords[0]) && userResponse.contains(desk.getName()) && desk.getEmpty() == false){
+                    System.out.println(desk.getDescription());
+                    deskPaper.setAccess(true); // allows user to pick up the paper
+                } else if(actionsList.contains(userWords[0]) && userResponse.contains(desk.getName()) && desk.getEmpty()){
+                    System.out.println(desk.getDescription2());
+                }
 
                 /* Add paper to inventory */
                 if(actionsList.contains(userWords[0]) && userResponse.contains("paper") && deskPaper.getAccess()){
                     System.out.println("This is going to be a method from the person class where they check if its not already in their inventory and then add it to their inventory");
                     user.getInventory().add("Paper");
                     System.out.println("Putting the piece of paper in pockets...");
+                    desk.setEmpty(true);
                 }
 
                 /* Go through black door to main bathroom */
@@ -172,6 +180,7 @@ public class Main{
                 if(actionsList.contains(userWords[0]) && userResponse.contains("toothbrush") && toothBrush.getAccess()){
                     user.getInventory().add("Toothbrush");
                     System.out.println("Putting the toothbrush in pockets...");
+                    
                 }
 
                 if(actionsList.contains(userWords[0]) && userResponse.contains("toothpaste") && toothPaste.getAccess()){
--- SubRoom.java
+++ SubRoom.java
@@ -1,17 +1,21 @@
-/* Items are only found in sub rooms */
+import java.util.List;
+
 public class SubRoom extends Room{
 
     // Attributes:
-    String description2; // secondary description of the area after the item has been taken
-    Room room; // Room the sub room is in
-    Boolean empty = false; // false means that there are items in the subroom
+    private String description2; // secondary description of the area after the item has been taken
+    private Room room; // Room the sub room is in
+    private Boolean empty = false; // false means that there are items in the subroom
+    private Item item;
+    private List<Item> itemList; // when there are more than 1 item in the sub room, they will be listed in this list
 
     // Constructor
-    public SubRoom(String name, Room room, String description, String description2, boolean locked, boolean empty){
+    public SubRoom(String name, Room room, String description, String description2, boolean locked, boolean empty, Item item, List<Item> itemList){
         super(name, description, false);
         this.room = room;
         this.description2 = description2;
         this.empty = empty;
+        this.itemList = itemList;
 
     }
 
@@ -28,11 +32,20 @@ public class SubRoom extends Room{
         return this.room;
     }
 
+    public Item getItem(){
+        return this.item;
+    }
+
+    /* if this is null there is only 1 item in the subroom */
+    public List<Item> getItemList(){
+        return this.itemList;
+    }
+
     public void setEmpty(boolean e){
         this.empty = e;
     }
 
-    
+
     
     
 }
